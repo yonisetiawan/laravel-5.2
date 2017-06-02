9d2cbc954ff271b163e5135b7601fcde375532fd
yonisetiawan <yonisetiawan1990@gmail.com>
2017-06-02 13:53:07 +0700

    create performRequest function

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,11 +2,16 @@
 
 namespace HttpClient\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
-
 use HttpClient\Http\Requests;
 
 class ClientController extends Controller
 {
-    //
+    protected function performRequest($method, $url, $parameter = [])
+    {
+      $client = new Client;
+      $response = $client->request($method, $url, $parameter);
+      
+    }
 }
