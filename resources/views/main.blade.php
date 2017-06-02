3df62c51ccd38996ed3093df962593ff35649425
yonisetiawan <yonisetiawan1990@gmail.com>
2017-06-02 13:15:16 +0700

    remove welcome blade and create new layout main

---
+++ resources/views/main.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('content')
+  <ul class="list-group">
+      <li class="list-group-item"><a href="#">Show all the Students</a></li>
+      <li class="list-group-item"><a href="#">Show all the Teachers</a></li>
+      <li class="list-group-item"><a href="#">Show all the Courses</a></li>
+  </ul>
+@endsection
