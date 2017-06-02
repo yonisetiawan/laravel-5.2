0c27e236ed3ee1f3f8dfb1fe39cbcd305f75fb6a
yonisetiawan <yonisetiawan1990@gmail.com>
2017-06-02 15:00:46 +0700

    add obtainAllStudents()

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -8,15 +8,21 @@ use HttpClient\Http\Requests;
 
 class ClientController extends Controller
 {
-    protected function performRequest($method, $url, $parameter = [])
+    protected function performRequest($method, $url, $parameters = [])
     {
       $client = new Client(['curl' => [CURLOPT_CAINFO => base_path('resources/certs/cacert.pem') ]]);
-      $response = $client->request($method, $url, $parameter);
+      $response = $client->request($method, $url, $parameters);
       return $response->getBody()->getContens();
     }
-    
-    protected function performGetRequest($url, $parameters = [])
-    {
 
+    protected function performGetRequest($url)
+    {
+        $this->performRequest('GET', $url);
+    }
+
+    // Function for students
+    protected function obtainAllStudents()
+    {
+      $contents = $this->performGetRequest('https://lumenapi.juandmegon.com/students');
     }
 }
